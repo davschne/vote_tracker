fdcb568535e5bc18dce4bef5365120f65707f546
davschne <davschne@gmail.com>
2015-05-26 16:06:43 -0700

    Repeated voting

--- script/kittens.js
+++ script/kittens.js
@@ -35,6 +35,25 @@ function Tracker() {
 
   };
 
+  this.init = function() {
+    // get image container nodes, remove highlighting
+    $photos = $('.photos').removeClass("highlight");
+
+    // hide/disable vote-again
+    $('.vote-again').hide();
+    $('.vote-again button').off("click");
+
+    // load photos
+    var indices = getRandomIndices();
+    imgA = photos[indices[0]];
+    imgB = photos[indices[1]];
+    imgA.display($photos.eq(0));
+    imgB.display($photos.eq(1));
+
+    // register event listener for voting
+    $('.photos').on("click", win);
+  };
+
   var win = function(event) {
     var $element = $(event.target).parent();
 
@@ -55,28 +74,12 @@ function Tracker() {
     // disable further voting
     $('.photos').off("click");
 
-    // show button to vote again
-
-  };
-
-  this.init = function() {
-    // get image container nodes
-    $photos = $('.photos').removeClass("highlight");
-
-    // remove highlighting
-
-    // hide vote-again button
-
-
-    // load photos
-    var indices = getRandomIndices();
-    imgA = photos[indices[0]];
-    imgB = photos[indices[1]];
-    imgA.display($photos.eq(0));
-    imgB.display($photos.eq(1));
-
-    // register event listener for voting
-    $('.photos').on("click", win);
+    // allow voting again
+    $('.vote-again').show();
+    $('.vote-again button').on("click", function(event) {
+      event.preventDefault();
+      tracker.init();
+    });
   };
 }
 
